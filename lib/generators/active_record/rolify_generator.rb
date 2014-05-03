4f7c0f9efa5cf6c3146d427612efd8e1bcd2961e
Johan Henkens <Johan@henkens.com>
2014-05-02 23:00:14 -0700

    Changed generators to allow multiple user classes

--- lib/generators/active_record/rolify_generator.rb
+++ lib/generators/active_record/rolify_generator.rb
@@ -5,8 +5,8 @@ module ActiveRecord
   module Generators
     class RolifyGenerator < ActiveRecord::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
-      
-      argument :user_cname, :type => :string, :default => "User", :banner => "User"
+
+      argument :user_cnames, :type => :array, :banner => "User UserClass2 ...", :default => ["User"]
       
       def generate_model
         invoke "active_record:model", [ name ], :migration => false
@@ -19,31 +19,48 @@ module ActiveRecord
       def copy_rolify_migration
         migration_template "migration.rb", "db/migrate/rolify_create_#{table_name}.rb"
       end
-      
-      def join_table
-        user_cname.constantize.table_name + "_" + table_name
+
+      def migration_join_table_params
+        if block_given?
+          user_tables = join_tables.zip(user_references)
+          user_tables.each do |table|
+            yield *table
+          end
+        end
       end
-      
-      def user_reference
-        user_cname.demodulize.underscore
+
+      def join_tables
+        user_cnames.map{ |x| x.constantize.table_name + "_" + table_name }
+      end
+
+      def user_references
+        user_cnames.map{ |x| x.demodulize.underscore }
+      end
+
+      def user_table_names
+        user_cnames.map{|x| x.constantize.table_name }
       end
       
       def role_reference
         class_name.demodulize.underscore
       end
-      
+
       def model_path
         File.join("app", "models", "#{file_path}.rb")
       end
       
       def model_content
-        content = <<RUBY
-  has_and_belongs_to_many :%{user_cname}, :join_table => :%{join_table}
+        content = "\n"
+        user_table_names.zip(join_tables).each do |user_cname, join_table|
+          content += "  has_and_belongs_to_many :#{user_cname}, :join_table => :#{join_table}\n"
+        end
+        content += <<RUBY
+
   belongs_to :resource, :polymorphic => true
   
   scopify
 RUBY
-        content % { :user_cname => user_cname.constantize.table_name, :join_table => "#{user_cname.constantize.table_name}_#{table_name}"}
+        content
       end
     end
   end
